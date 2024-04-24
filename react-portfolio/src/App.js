91d926c870c99494bc1cdac7fe3d309270c931ed
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-04-23 22:16:50 -0700

    roliing along

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 
 
@@ -9,10 +11,18 @@
 
 
 function App() {
-  return (
-    <div className="App">
+  const [selectedPage, setSelectedPage] = useState("home")
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return 
+    <div className="app bg-deep-blue">
+      <Navbar
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}
+      />
+
     </div>
-  );
+  
 }
 
 export default App;
