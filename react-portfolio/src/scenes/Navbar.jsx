e249120838b5b37e886cb9cc9772a8e7ae182178
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-04-26 10:39:33 -0700

    roliing along2

--- react-portfolio/src/scenes/Navbar.jsx
+++ react-portfolio/src/scenes/Navbar.jsx
@@ -1,16 +1,40 @@
-import {useState} from 'react';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 
-const Navbar = ({selectedPage, setSelectedPage}) => {
-    const [isMenuToggled, setIsMenuToggled] = useState(false);
-    const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+const Link = ({page, selectedPage, setSelectedPage}) =>{
+    const lowerCasePage = page.toLowerCase();
+    return (
+        <AnchorLink className={`${selectedPage === lowerCasePage ? "text-yellow":"text-white"}
+        hover:text-yellow transition duration-500`}
+        href={`#${lowerCasePage}`}
+        onClick ={() => setSelectedPage(lowerCasePage)}
 
-    return(
-        <nav className={`z-40 w-full fixed top-0 py-6`}>
-            <div className="flex items-center"
+        >
 
-
-        </nav>
+        </AnchorLink>
     )
 }
+const Navbar = ({ selectedPage, setSelectedPage }) => {
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+
+  return (
+    <nav className={`z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+<h4 className="font-playfair text-3xl font-bold">JE</h4>
+
+       {/* DESKTOPNAV  */}
+       {isAboveSmallScreens ? (
+        <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+            page="Home"
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage} 
+            />
+        </div>
+       ):(<div></div>)}
+     </div>
+    </nav>
+  );
+};
