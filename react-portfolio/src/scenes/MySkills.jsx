4fb7599f52e582dee41813b3ff918f92dcdd988f
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-06-27 10:22:23 -0700

    dots4 for shame

--- react-portfolio/src/scenes/MySkills.jsx
+++ react-portfolio/src/scenes/MySkills.jsx
@@ -107,7 +107,7 @@ const MySkills = () => {
         </motion.div>
         {/* IMAGINATIVE */}
         <motion.div
-        //derp
+        //derp serious
           className="md:w-1/3 mt-10"
           initial="hidden"
           whileInView="visible"
